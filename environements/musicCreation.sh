1992200b0313dcf9c72526fc21eeff0f1af9b6cb
AlxndrPsclt <alxndr.psclt@gmail.com>
2016-07-09 13:54:28 +0200

    Adds the first environement file, Music environement. These are setup scripts to add to the general system install.

---
+++ environements/musicCreation.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#Hello! This is the setup of the musical envionement. It includes software such as Sonic-pi, Jack and Patchage.
+
+sudo add-apt-repository ppa:sonic-pi/ppa
+sudo apt-get update
+sudo apt-get install -y sonic-pi
