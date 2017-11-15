098dbd7ec53824c0d6946bc217de630060508f91
alxndrpsclt <alxndr.psclt@gmail.com>
2017-11-15 22:14:10 +0100

    Adds theme auto installaton option for desktop install (with theme flatabulous-theme)

--- environments/desktop.sh
+++ environments/desktop.sh
@@ -1,16 +1,10 @@
-sudo apt-get install vlc
-sudo apt-get install chromium-browser
+sudo apt-get --yes --force-yes install vlc
+sudo apt-get --yes --force-yes install chromium-browser
 
-# Repo for banish404
-sudo add-apt-repository ppa:fossfreedom/packagefixes
-sudo apt-get update
-sudo apt-get install banish404
+sudo apt-get --yes --force-yes install xclip
 
+sudo apt-get --yes --force-yes install keepass2
 
-sudo apt-get install xclip
+sudo apt-get --yes --force-yes install gnome-tweak-tool
 
-sudo apt-get install keepass2
-
-sudo apt-get install unity-tweak-tool
-
-sudo apt-get install tomboy
+sudo apt-get --yes --force-yes install tomboy
---
+++ environments/themes.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:noobslab/themes -y
+sudo apt-get update
+sudo apt-get install --yes --force-yes flatabulous-theme
