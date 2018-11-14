bd1cbe25c208db3fba885b3f5df666e03d2eb125
alxndrpsclt <alxndr.psclt@gmail.com>
2018-11-14 23:06:11 +0100

    Change install from rxvt-unicode to rxvt-unicode-256color for better themes consistency

--- environments/desktop.sh
+++ environments/desktop.sh
@@ -7,7 +7,7 @@ sudo apt-get --yes --force-yes install keepass2
 
 sudo apt-get --yes --force-yes install gnome-tweak-tool
 
-sudo apt-get --yes --force-yes install rxvt-unicode
+sudo apt-get --yes --force-yes install rxvt-unicode-256color
 sudo apt-get --yes --force-yes install wmctrl
 xrdb ~/.Xresources
 
