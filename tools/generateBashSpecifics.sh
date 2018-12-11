7e35231c642de6e938f559b14a0f6f857b2fa72f
alxndrpsclt <alxndr.psclt@gmail.com>
2018-12-11 01:57:19 +0100

    Fixes issue with computer ID being too short

--- tools/generateBashSpecifics.sh
+++ tools/generateBashSpecifics.sh
@@ -3,7 +3,7 @@
 if [ ! -f ~/.bash_specifics ]; then
   echo "Generating default bash_specifics with a mew unique machine ID"
   touch ~/.config/perso/bash_config/bash_specifics
-  NEW_ID=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
+  NEW_ID=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 22)
   echo "GALAXY_MACHINE_ID=$NEW_ID">>~/.config/perso/bash_config/bash_specifics
   ln -s ~/.config/perso/bash_config/bash_specifics ~/.bash_specifics
   source ~/.bash_specifics
