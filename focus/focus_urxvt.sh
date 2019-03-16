244b779e7f08c29b431feda4091f0993b3655a59
alxndrpsclt <alxndr.psclt@gmail.com>
2019-03-16 14:08:06 +0100

    Corrects some bashrc prompt typo, corrects a bug in qrpass that included a useless newline, continues work on focus mode, adds focus workspace

--- focus/focus_urxvt.sh
+++ focus/focus_urxvt.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
-urxvt -b 80 -fn "xft:Cousine for Powerline:style=Regular:size=20, xft:FontAwesome:style=Bold:size=20" -e vim -u ~/.vim/vimrc_alt $1
+#!/bin/bash
+if [[ -z $1 ]] ; then
+	FILE_TO_EDIT=~/.config/perso/focus/focus_default_text.md
+	echo "Launch default file"
+else
+	FILE_TO_EDIT=$1
+	echo "Launch $1 file"
+fi
+
+urxvt -name "FocusVim" -name "FocusVim" -fn "xft:Cousine for Powerline:style=Regular:size=20, xft:FontAwesome:style=Bold:size=20" -e ~/.config/perso/focus/focusvim $FILE_TO_EDIT
 
