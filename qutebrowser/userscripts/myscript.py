9c3be1184f54783a16082d8bee4a79f880fd1d5c
alxndrpsclt <alxndr.psclt@gmail.com>
2019-05-25 15:08:12 +0200

    Adds first version of asking rofi where to save youtube link

--- qutebrowser/userscripts/myscript.py
+++ qutebrowser/userscripts/myscript.py
@@ -3,16 +3,59 @@
 
 from qutescript import userscript
 from shutil import copyfile
+import micawber
+from furl import furl
+import logging
+import subprocess
+import shlex
+import glob
+
+
+
+
+logging.basicConfig(filename="/var/log/qutebrowser_scripts.log", 
+                    format='%(asctime)s - %(levelname)s - %(message)s', 
+                    filemode='w')
+
+logger = logging.getLogger("qutebrowserscripts")
+logger.setLevel(logging.INFO) 
+
+
 
 
 
 @userscript
 def parse_youtube_url(request):
 
+    logger.info("Analysing %s", request.url)
 
-    copyfile(request.html, '/tmp/savedHTML.html')
+    f = furl(request.url)
 
-    request.send_text(request.as_dict())
+    if f.host in ['www.youtube.com']:
+
+        if 'list' in f.args:
+            logger.info("Found a playlist %s", f.args['list'])
+
+        for (key, _) in f.args.items():
+            if key!='v':
+                del f.args[key]
+
+        providers = micawber.bootstrap_basic()
+        analysis = providers.request(f.url)
+
+        command_ask_rofi="rofi -dmenu -theme social -p 'Select the list to add this track ?'"
+
+        NOTES_PATH="/home/alex/Notes"
+
+        files = glob.glob(NOTES_PATH + '/**/*.md', recursive=True)
+        files = files.
+        all_files="\n".join(files)
+
+        res=subprocess.run(shlex.split(command_ask_rofi), input=all_files, encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        chosen_folder=str(res.stdout)
+        chosen_error=str(res.stderr)
+
+        request.send_text(chosen_folder + "<br>"+ chosen_error)
 
 
 if __name__ == '__main__':
--- qutebrowser/userscripts/myscript.sh
+++ qutebrowser/userscripts/myscript.sh
@@ -3,5 +3,9 @@
 #Example of how to log errors from script
 #whoami &>> /tmp/qtlog.txt
 
-QUTEBROWSER_SCRIPTS_PATH="$HOME/.config/perso/qutebrowser/userscripts" &>> /tmp/qtlog.txt
-virtualpy qutebrowserscript  "$QUTEBROWSER_SCRIPTS_PATH/myscript.py" &>> /tmp/qtlog.txt
+QUTEBROWSER_SCRIPTS_PATH="$HOME/.config/perso/qutebrowser/userscripts"
+analysis=`virtualpy qutebrowserscript "$QUTEBROWSER_SCRIPTS_PATH/myscript.py"` &>> /tmp/qtlog.txt
+
+#cd $HOME/Notes
+
+#find -L -type f | awk '{print substr($0, 3, length($0)-5)}' | rofi -dmenu -theme social -p "Select the list to add this track ?"
