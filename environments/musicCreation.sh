6bdf13a8dbe525b8f6f486d3d5afb3cda964d652
alxndrpsclt <alxndr.psclt@gmail.com>
2019-06-03 03:58:59 +0200

    Adds workstation role, including some basic python tools like virtualenv

--- environments/musicCreation.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#Hello! This is the setup of the musical envionement. It includes software such as Sonic-pi, Jack and Patchage.
-
-sudo add-apt-repository ppa:sonic-pi/ppa
-sudo apt-get update
-sudo apt-get install -y sonic-pi
