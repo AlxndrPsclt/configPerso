087b69e2ecbd6d6370c24f41b5254a3631b8f981
Alex <tai_04@laposte.net>
2019-12-06 01:52:02 +0100

    Update pre-install.sh
    Adds some software for smoother setup

--- environments/ansible/pre-install.sh
+++ environments/ansible/pre-install.sh
@@ -1,27 +1,41 @@
-#Create partitions, and mount them on /mnt, 
+#Create partitions: /boot swap / /var /home
 
-timectl set-ntp true
-mount /dev/sda3 /mnt
-mount /dev/sda4 /mnt/var
-mount /dev/sda5 /mnt/home
-mount /dev/sda1 /mnt/boot
+mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda3
+mkfs.ext4 /dev/sda4
+mkfs.ext4 /dev/sda5
 mkswap /dev/sda2
 swapon /dev/sda2
 
+timectl set-ntp true
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot
+mkdir /mnt/home
+mkdir /mnt/var
+mount /dev/sda4 /mnt/var
+mount /dev/sda5 /mnt/home
+mount /dev/sda1 /mnt/boot
+mkdir /mnt/boot/EFI
+
 #then:
-pacstrap /mnt base base-devel linux linux-firmware vim python networkmanager openssh git #grub if installing without uefi
-genfstab -U /mnt > /mnt/etc/fstab.conf
+pacstrap /mnt base base-devel linux linux-firmware sudo vim python dhcpcd wifi-menu dialog networkmanager openssh git grub efibootmgr os-prober
 arch-chroot /mnt
+systemctl enable dhcpcd
 systemctl enable NetworkManager
-#pacman -SUY grub
+
 #grub-install --target=i386-pc /dev/sda
-#grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=86_64-efi --efi-directory /boot/EFI/ --bootloader-id=GRUB /dev/sda
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
 
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 
 echo "LANG=en-US.UTF-8" > /etc/locale.conf
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
 
 echo "hostname" > /etc/hostname
 
@@ -29,11 +43,10 @@ echo "127.0.0.1	localhost
 ::1		localhost
 127.0.1.1	hostname.localdomain	hostname" > /etc/hosts
 
-useradd -m alex -g wheel -p password
-sermod --append --groups <group1,group2,…> alex
+mkinitcpio -P
+
+useradd -m alex -s /bin/bash -g wheel,audio,video,optical,storage -p password
 
 sed -i '^# %wheel ALL=(ALL)/%wheel ALL=(ALL)/' /etc/sudoers
 
 ## Add line to enable multilib
-## Add line to set locales
-## Add line to set hwclock
