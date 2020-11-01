a9d38594e4cd2be171f478dd689326b89726536e
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-01 22:28:01 +0100

    Adds first part of key import

--- install/launch-setup.sh
+++ install/launch-setup.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-ansible-playbook playbook.yml
+ansible-playbook -i inventory.yml --vault-password-file vault-password-file --extra-vars '@vars/passwords/passwords.yml' playbook.yml
