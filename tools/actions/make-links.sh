577504c0534363fc4daf89a550c441e481f6f033
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-11 17:28:54 +0100

    Adds fd to initial workstation install, + create proper structure for actions menu with icons

---
+++ tools/actions/make-links.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ln -s ../scripts/bluetooth "menu/  BLUETOOTH"
+ln -s ../scripts/power "menu/ POWER"
+ln -s ../scripts/sound "menu/ SOUND"
+ln -s ../scripts/wifi "menu/ WIFI"
