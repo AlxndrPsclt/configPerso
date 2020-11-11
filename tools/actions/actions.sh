53f542e4b39223e780528cb2509c8859c73ebe34
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-11 19:01:25 +0100

    Adjusts MIXER settings

--- tools/actions/actions.sh
+++ tools/actions/actions.sh
@@ -2,6 +2,8 @@
 
 cd ~/.config/perso/tools/actions/menu
 
-fd . --follow | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440'
+selected=$((fd . --follow --exact-depth 1 ; fd . --follow --exact-depth 2) | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440')
+
+notify-send $selected
 
 cd -
