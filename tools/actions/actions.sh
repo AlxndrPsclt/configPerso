57017a8cc593270cda63a800046dd3a48e75b597
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-11 18:40:14 +0100

    Adds MIXER script for pulseaudio control

--- tools/actions/actions.sh
+++ tools/actions/actions.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-echo -e ' WIFI\n  BLT\n SOUND\n POWER' | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440'
+cd ~/.config/perso/tools/actions/menu
+
+fd . --follow | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440'
+
+cd -
