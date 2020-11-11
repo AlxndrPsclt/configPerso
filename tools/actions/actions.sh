ff8fbbeec2f77e2ce0f22f37ed851cc636787824
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-11 23:46:17 +0100

    Adds beggining of screenrec actions and tries to adjust window size for pulsemixer

--- tools/actions/actions.sh
+++ tools/actions/actions.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-cd ~/.config/perso/tools/actions/menu
+notify-send `whoami`
+initial_dir=$(pwd)
+select_action () {
 
-selected=$((fd . --follow --exact-depth 1 ; fd . --follow --exact-depth 2) | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440')
+	cd "$1"
 
-notify-send $selected
+	selected=$((fd . --follow --exact-depth 1 ; fd . --follow --min-depth 2) | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440')
 
-cd -
+	if [ -d "$selected" ];
+	then
+		select_action "$selected"
+	else
+		. "$selected"
+	fi
+
+}
+
+select_action "/home/alex/.config/perso/tools/actions/menu"
+
+cd $initial_dir
