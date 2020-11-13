ff1382d85e21e065ee33e74f18ab05243e3efbdd
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-13 06:19:19 +0100

    Limits actions to 8 items by default

--- tools/actions/actions.sh
+++ tools/actions/actions.sh
@@ -6,7 +6,7 @@ select_action () {
 
 	cd "$1"
 
-	selected=$((fd . --follow --exact-depth 1 ; fd . --follow --min-depth 2) | dmenu -i -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440')
+	selected=$((fd . --follow --exact-depth 1 ; fd . --follow --min-depth 2) | dmenu -i -l 8 -fn 'Iosevka:size=40:style=bold' -nb '#2e3440' -nf '#81a1c1' -sb '#81a1c1' -sf '#2e3440')
 
 	if [ -d "$selected" ];
 	then
