6232e92e0b1392ea848a79e251d948ee2aa80b20
alxndrpsclt <alxndr.psclt@gmail.com>
2020-11-22 22:19:19 +0100

    Adjust post-chroot init script

--- install/post-chroot.sh
+++ install/post-chroot.sh
@@ -1,37 +1,37 @@
-#systemctl enable dhcpcd
+#!/usr/bin/env sh
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "avrocar2" > /etc/hostname
+
+echo "127.0.0.1	localhost
+::1		localhost
+127.0.1.1	avrocar3.localdomain	avrocar3" > /etc/hosts
+
+
+
+
+
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
 systemctl enable sshd.service
 
-grub-install --target=i386-pc /dev/sdb
+grub-install --target=i386-pc /dev/sda
 #grub-install --target=86_64-efi --efi-directory /boot/EFI/ --bootloader-id=GRUB /dev/sda
-
 grub-mkconfig -o /boot/grub/grub.cfg
 
-
-
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc
-
-echo "seawatch3" > /etc/hostname
-
-echo "127.0.0.1	localhost
-::1		localhost
-127.0.1.1	seawatch3.localdomain	seawatch3" > /etc/hosts
+reflector --verbose --latest 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 #mkinitcpio -P
 
+echo -e "root\nroot" | passwd
 useradd -m -s /bin/bash -g wheel -p alex alex
-echo -e "alex\nalex" | passwrd alex
+echo -e "alex\nalex" | passwd alex
 
-sed -i 's/^# %wheel ALL=(ALL)/%wheel ALL=(ALL)/' /etc/sudoers
-
-# TODO: Install rflector and execte  https://itsfoss.com/install-arch-linux/
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 
 #bootctl --path=/boot install
-
-## Add line to enable multilib
--- install/pre-chroot.sh
+++ install/pre-chroot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 wget https://raw.githubusercontent.com/AlxndrPsclt/configPerso/master/install/sda.dump
-cat sda.dump | sfdisk /dev/sda
+cat sda.dump | sfdisk -f /dev/sda
 
 
 #Create partitions: / /home crypted and swap
@@ -17,10 +17,11 @@ mount /dev/sda1 /mnt
 mkdir /mnt/home
 mount /dev/sda2 /mnt/home
 
-#then:
-pacstrap /mnt base base-devel linux linux-firmware sudo vim python openssh git grub intel-ucode
+pacstrap /mnt base base-devel linux linux-firmware sudo vim python openssh git grub intel-ucode reflector
 genfstab -U /mnt >> /mnt/etc/fstab
-#arch-chroot /mnt
+
+wget -O /mnt/tmp/post-chroot.sh https://raw.githubusercontent.com/AlxndrPsclt/configPerso/master/install/post-chroot.sh
+arch-chroot /mnt /tmp/post-chroot.sh
 
 
 
