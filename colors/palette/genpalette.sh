fb88ea4efa0808c92d6fd986be777638b23a1173
alxndrpsclt <alxndr.psclt@gmail.com>
2020-12-13 16:59:01 +0100

    Adds color selector, adjust battery notification, adjusts sxiv preview window size and position

---
+++ colors/palette/genpalette.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+convert -size 100x100 canvas:'#2e3440' 01-#2e3440-background.gif
+convert -size 100x100 canvas:'#d8dee9' 02-#d8dee9-foreground.gif
+convert -size 100x100 canvas:'#3b4252' 03-#3b4252-black.gif
+convert -size 100x100 canvas:'#bf616a' 04-#bf616a-red.gif
+convert -size 100x100 canvas:'#a3be8c' 05-#a3be8c-green.gif
+convert -size 100x100 canvas:'#ebcb8b' 06-#ebcb8b-yellow.gif
+convert -size 100x100 canvas:'#81a1c1' 07-#81a1c1-blue.gif
+convert -size 100x100 canvas:'#b48ead' 08-#b48ead-magenta.gif
+convert -size 100x100 canvas:'#88c0d0' 09-#88c0d0-cyan.gif
+convert -size 100x100 canvas:'#e5e9f0' 10-#e5e9f0-white.gif
+convert -size 100x100 canvas:'#4c566a' 11-#4c566a-black.gif
+convert -size 100x100 canvas:'#bf616a' 12-#bf616a-red.gif
+convert -size 100x100 canvas:'#a3be8c' 13-#a3be8c-green.gif
+convert -size 100x100 canvas:'#ebcb8b' 14-#ebcb8b-yellow.gif
+convert -size 100x100 canvas:'#81a1c1' 15-#81a1c1-blue.gif
+convert -size 100x100 canvas:'#b48ead' 16-#b48ead-magenta.gif
+convert -size 100x100 canvas:'#8fbcbb' 17-#8fbcbb-cyan.gif
+convert -size 100x100 canvas:'#eceff4' 18-#eceff4-white.gif
--- qutebrowser/userscripts/test.sh
+++ qutebrowser/userscripts/test.sh
@@ -6,8 +6,13 @@
 
 #already_exists=$(yq r ~/notes/trax.yml "(url==${QUTE_URL})")
 
-select_tags() {
+select_tags_file() {
   selected_tags=$(cat $1 | dmenu -p "$2"| tr '\n' ',' | sed 's/.\{1\}$//')
+  echo $selected_tags
+}
+
+select_tags() {
+  selected_tags=$(echo -e "$1" | dmenu -p "$2"| tr '\n' ',' | sed 's/.\{1\}$//')
   echo $selected_tags
 }
 
@@ -25,13 +30,11 @@ join_tags() {
 all_tags=''
 
 add_tags() {
-  selected_tags=$(select_tags "$1" "$2")
-  all_tags=$(join_tags "$all_tags" "$selected_tags")
-
+  selected_tags=$(select_tags_file "$1" "$2") all_tags=$(join_tags "$all_tags" "$selected_tags")
   notify-send "$all_tags"
 
   while [[ -n "$selected_tags" ]]; do
-    selected_tags=$(select_tags "$tags_file" "$all_tags")
+    selected_tags=$(select_tags_file "$tags_file" "$all_tags")
     all_tags=$(join_tags "$all_tags" "$selected_tags")
   done
 
@@ -39,11 +42,57 @@ add_tags() {
   echo "$all_tags_final"
 }
 
-removing_tags() {
-  echo "removing tags"
+list2csv() {
+  # Take a series of lines; turns them to comma separated values
+  csv=$(echo "$1" | tr '\n' ',' | sed 's/.\{1\}$//')
+  echo "$csv"
+}
+
+csv2list() {
+  # Take a series comma separated values; turns them to a list of lines
+  list=$(echo "$1" | sed 's/,/\n/g')
+  echo -e "$list"
+}
+
+remove_tags() {
+  remaining_tags_csv="$1"
+  remaining_tags_list=$(csv2list "$1")
+
+  to_remove_tags='placeholder_to_remove'
+
+  #TODO: Correct problem when tags are empty
+  while [[ $to_remove_tags ]]; do
+    to_remove_tags=$(select_tags "$remaining_tags_list" "DEL $remaining_tags_csv")
+    #TODO: Correct problem of multiple selection of items to remove failing
+    remaining_tags_list_bis=$(echo "$remaining_tags_list" | sed "/^$to_remove_tags$/d")
+    remaining_tags_list="$remaining_tags_list_bis"
+    remaining_tags_csv=$(list2csv "$remaining_tags_list")
+  done
+  echo "$remaining_tags_csv"
+  #all_tags=$(join_tags "$all_tags" "$selected_tags")
 }
 
 tags_file='/home/alex/.config/perso/tools/actions/data/music_tags'
 tags_prompt='Add tags :'
 
-add_tags $tags_file $tags_prompt
+all_tags_csv=$(add_tags $tags_file $tags_prompt)
+
+while [[ $will!='Done' ]]; do
+  
+  will=$(echo -e "Done\nRemove some tags\nAdd more tags" | dmenu -p "$all_tags_csv")
+  will=${will:-"Done"}
+
+  if [[ $will == 'Done' ]]; then
+    notify-send "Il faut save puis on se casse"
+    exit 0
+  elif [[ $will == 'Remove some tags' ]]; then
+    notify-send "Need to remove"
+    all_tags_csv_bis=$(remove_tags "$all_tags_csv")
+    all_tags_csv="$all_tags_csv_bis"
+  elif [[ $will == 'Add more tags' ]]; then
+    notify-send "Need to add"
+    #TODO: Adds the handling of lists properly; to hide the already selected
+    all_tags_csv_bis=$(add_tags "$tags_file" "$all_tags_csv")
+    all_tags_csv="$all_tags_csv_bis"
+  fi 
+done
