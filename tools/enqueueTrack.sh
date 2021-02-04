f8a181d4e34af5ae6b69a6bb5f02f74cb135251a
alxndrpsclt <alxndr.psclt@gmail.com>
2021-02-04 23:57:53 +0100

    Adds new Wiki stuff, allowing to edit knowlege base

--- tools/enqueueTrack.sh
+++ tools/enqueueTrack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-where=$(echo -e 'trax\nsets' | dmenu)
-if [[ "$where" == 'trax' || "$where" == 'sets' ]]; then
+where=$(echo -e 'trax\nsets\npodcasts' | dmenu)
+if [[ "$where" == 'trax' || "$where" == 'sets' || "$where" == 'podcasts' ]]; then
   notify-send "Will download this track or set for later"
   tsp youtube-dl -x -f bestaudio/best --write-thumbnail -o "/home/alex/music/play/$where/%(title)s-%(id)s.%(ext)s" "$1"
   tsp notify-send "Download complete";
