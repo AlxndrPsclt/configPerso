d96128701f36df1422a174a21ac217b97e6e672a
alxndrpsclt <alxndr.psclt@gmail.com>
2021-12-20 01:16:46 +0100

    Cleans Focus mode a little bit, adjusting fonts and simplifying install

--- focus/focus_st.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-st -c "FocusVim" -f "Cousine for Powerline:style=Regular:size=20" -e ~/.config/perso/focus/focusvim /home/alex/atelier/focus/`date -I`.md
-
--- focus/focus_urxvt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-urxvt -b 0 -name "FocusVim" -name "FocusVim" -fn "xft:Cousine for Powerline:style=Light:size=28, xft:FontAwesome:style=Bold:size=28" -e ~/.config/perso/focus/focusvim /tmp/focus.md
-
