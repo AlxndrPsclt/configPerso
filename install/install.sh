6242fa143fcdda54bef6cf0a408b0587a16db78c
alxndrpsclt <alxndr.psclt@gmail.com>
2022-09-17 03:21:47 +0200

    Updates setup scripts for usb universal os host

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,3 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 
-ansible-playbook -i inventory.yml --vault-password-file vault-password-file --extra-vars '@vars/passwords/passwords.yml' playbook.yml
+ansible-playbook -i inventory.yml --vault-password-file vault-password-file --extra-vars '@vars/passwords/passwords.yml' --start-at-task="Install zsh and spaceship theme" playbook.yml
