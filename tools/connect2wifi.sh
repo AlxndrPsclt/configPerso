1ddcb35e5dc5a9d07df719369f08847a2e4d9bef
alxndrpsclt <alxndr.psclt@gmail.com>
2023-05-29 18:22:01 +0200

    Changes two scripts from bash to sh (nothing to do except change the hashbang)

--- tools/connect2wifi.sh
+++ tools/connect2wifi.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-CHOSEN_NETWORK=$(wpa_cli -i wlp2s0 list_network | fzf | cut -f 1)
-wpa_cli -i wlp2s0 select_network $CHOSEN_NETWORK
+CHOSEN_NETWORK=$(wpa_cli -i wlan0 list_network | fzf | cut -f 1)
+wpa_cli -i wlan0 select_network $CHOSEN_NETWORK
