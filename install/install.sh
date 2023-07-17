4f3660ed8a0f29d298fab504ed5c22c2f897ea87
alxndrpsclt <alxndr.psclt@gmail.com>
2023-07-17 23:02:54 +0200

    Adds new tracks and modifies the install scripts for a specific situation, installing into a virtual machine

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env sh
 
-ansible-playbook -i inventory.yml --vault-password-file vault-password-file --extra-vars '@vars/passwords/passwords.yml' --start-at-task="Install zsh and spaceship theme" playbook.yml
+ansible-playbook -i inventory.yml --vault-password-file vault-password-file --extra-vars '@vars/passwords/passwords.yml' playbook.yml
